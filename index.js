fedb3881d0ab0365ee3f55eb90bdeb3b551c806c
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-02-01 02:18:09 +0200

    home page done-responsive??

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 window.addEventListener('load', () => {
     document.getElementById('phrase').classList.add('show');
 });
+function downloadCV(){
+     window.open("files/Mahlatse_CV.pdf", "_blank");
+}
